57e630b2adab553534cc81c908f97e3f5a77b5e8
jonas.liljenfeldt <jonas.liljenfeldt@8bcfdb14-2e69-11df-8b4d-31f007950f82>
2010-03-22 12:45:23 +0000

    Updated wizard labels to reflect Pivotal Tracker settings.

--- com.pt-connector.ui/src/com/pt_connector/ui/editor/PivotalTrackerTaskEditorPageFactory.java
+++ com.pt-connector.ui/src/com/pt_connector/ui/editor/PivotalTrackerTaskEditorPageFactory.java
@@ -1,56 +1,40 @@
 package com.pt_connector.ui.editor;
 
-import org.eclipse.mylyn.tasks.ui.ITasksUiConstants;
-import org.eclipse.mylyn.tasks.ui.TasksUiUtil;
 import org.eclipse.mylyn.tasks.ui.editors.AbstractTaskEditorPageFactory;
 import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
 import org.eclipse.mylyn.tasks.ui.editors.TaskEditorInput;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.forms.editor.IFormPage;
 
-import com.pt_connector.core.PivotalTrackerCorePlugin;
+public class PivotalTrackerTaskEditorPageFactory extends
+		AbstractTaskEditorPageFactory {
 
-public class PivotalTrackerTaskEditorPageFactory extends AbstractTaskEditorPageFactory {
+	public PivotalTrackerTaskEditorPageFactory() {
+		// TODO Auto-generated constructor stub
+	}
 
-    public PivotalTrackerTaskEditorPageFactory() {
-        // TODO Auto-generated constructor stub
-    }
+	@Override
+	public boolean canCreatePageFor(TaskEditorInput input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean canCreatePageFor(TaskEditorInput input) {
-        if (input.getTask().getConnectorKind().equals(PivotalTrackerCorePlugin.CONNECTOR_KIND)) {
-            return true;
-        } else if (TasksUiUtil.isOutgoingNewTask(input.getTask(), PivotalTrackerCorePlugin.CONNECTOR_KIND)) {
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public IFormPage createPage(TaskEditor parentEditor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public IFormPage createPage(TaskEditor parentEditor) {
-        return new PivotalTrackerTaskEditorPage(parentEditor, PivotalTrackerCorePlugin.CONNECTOR_KIND);
-    }
+	@Override
+	public Image getPageImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public String[] getConflictingIds(TaskEditorInput input) {
-        return new String[] { ITasksUiConstants.ID_PAGE_PLANNING };
-    }
-
-
-    @Override
-    public String getPageText() {
-        return "Trac"; //$NON-NLS-1$
-    }
-
-    @Override
-    public int getPriority() {
-        return PRIORITY_TASK;
-    }
-
-    @Override
-    public Image getPageImage() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public String getPageText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- com.pt-connector.ui/src/com/pt_connector/ui/wizard/PivotalTrackerRepsitorySettingsPage.java
+++ com.pt-connector.ui/src/com/pt_connector/ui/wizard/PivotalTrackerRepsitorySettingsPage.java
@@ -1,56 +1,81 @@
 package com.pt_connector.ui.wizard;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.mylyn.tasks.core.RepositoryTemplate;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.ui.wizards.AbstractRepositorySettingsPage;
 import org.eclipse.swt.widgets.Composite;
 
 import com.pt_connector.core.PivotalTrackerCorePlugin;
+import com.pt_connector.core.util.ReflectionUtil;
 
-public class PivotalTrackerRepsitorySettingsPage extends
-		AbstractRepositorySettingsPage {
+public class PivotalTrackerRepsitorySettingsPage extends AbstractRepositorySettingsPage {
 
-	public PivotalTrackerRepsitorySettingsPage(TaskRepository taskRepository) {
-		super("Pivotal Tracker Repository Settings", "Settings for Pivotal Tracker Repository", taskRepository);
-		setNeedsAnonymousLogin(false);
-		setNeedsEncoding(false);
-		setNeedsTimeZone(false);
-		setNeedsAdvanced(false);
-		setNeedsHttpAuth(false);
-	}
-		
-	@Override
-	public void createControl(Composite parent) {
-		super.createControl(parent);
-		addRepositoryTemplatesToServerUrlCombo();
-	}
-	
-	@Override
-	protected void repositoryTemplateSelected(RepositoryTemplate template) {
-		repositoryLabelEditor.setStringValue(template.label);
-		setUrl(template.repositoryUrl);
-		getContainer().updateButtons();
-	}
+    String serverUrl = "http://www.pivotaltracker.com/services/v3/";
 
-	@Override
-	protected void createAdditionalControls(Composite parent) {
-		// TODO Auto-generated method stub
-	}
+    public PivotalTrackerRepsitorySettingsPage(TaskRepository taskRepository) {
+        super("Pivotal Tracker Repository Settings", "Settings for Pivotal Tracker Repository", taskRepository);
+        setNeedsAnonymousLogin(false);
+        setNeedsEncoding(false);
+        setNeedsTimeZone(false);
+        setNeedsAdvanced(false);
+        setNeedsHttpAuth(false);
+    }
 
-	@Override
-	public String getConnectorKind() {
-		return PivotalTrackerCorePlugin.CONNECTOR_KIND;
-	}
+    @Override
+    public void createControl(Composite parent) {
+        super.createControl(parent);
+        addRepositoryTemplatesToServerUrlCombo();
+        if (repository == null) {
+            setUrl(serverUrl);
+        }
+        
+        // FIXME Not a very nice way of changing the label values.
+        try {
+            ReflectionUtil.setStaticFieldValue(AbstractRepositorySettingsPage.class, "LABEL_USER", "Project Id:");
+            ReflectionUtil.setStaticFieldValue(AbstractRepositorySettingsPage.class, "LABEL_PASSWORD", "API Token:");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        
+        savePasswordButton.setText("Save token");
+    }
+    
+    @Override
+    protected void repositoryTemplateSelected(RepositoryTemplate template) {
+        repositoryLabelEditor.setStringValue(template.label);
+        setUrl(template.repositoryUrl);
+        setUserId("user");
+        setPassword("pass");
+        getContainer().updateButtons();
+    }
 
-	@Override
-	protected Validator getValidator(TaskRepository repository) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected void createAdditionalControls(Composite parent) {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
-	protected boolean isValidUrl(String url) {
-		return url != null && url.trim().length() > 0;
-	}
+    @Override
+    public String getConnectorKind() {
+        return PivotalTrackerCorePlugin.CONNECTOR_KIND;
+    }
+
+    @Override
+    protected Validator getValidator(TaskRepository repository) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected boolean isValidUrl(String url) {
+        return url != null && url.trim().length() > 0;
+    }
 
 }
